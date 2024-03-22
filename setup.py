928dfd795d1fd46594afef51ed977a7300745d2a
Samuel O <samueladebare1010@gmail.com>
2024-03-22 07:33:06 +0300

    setup.py file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="pytimer",
+      version="1.0.0",
+      description="Helps to measure the time it takes for a function or method to run",
+      package=["pytimer"],
+      author="Samuel Oyewoade",
+      author_email="samueladebare1010@gmail.com",
+      zip_safe=False
+      )
